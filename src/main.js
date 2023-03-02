db655102ac9d3c5f08286a3c936fbb657df81e17
frankwang <frankwang95174@gmail.com>
2023-03-03 01:43:49 +0800

    add packages: boostrap, sweetalert2, vue-loading-overlay

--- src/main.js
+++ src/main.js
@@ -4,11 +4,18 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
-import './assets/main.css'
+import './assets/all.scss'
+import VueSweetalert2 from 'vue-sweetalert2';
+import 'sweetalert2/dist/sweetalert2.min.css';
+
+import {LoadingPlugin} from 'vue-loading-overlay';
+import 'vue-loading-overlay/dist/css/index.css';
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(VueSweetalert2);
+app.use(LoadingPlugin);
 
 app.mount('#app')
