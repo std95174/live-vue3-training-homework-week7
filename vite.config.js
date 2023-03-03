93d087a17cf00c1a82a0f1b357a179a566f4cbe1
frankwang <frankwang95174@gmail.com>
2023-03-03 23:29:37 +0800

    update for deployment

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: "/live-vue3-training-homework-week7/",
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
