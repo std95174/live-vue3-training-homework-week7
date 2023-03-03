7f406f1581200d2fadf935efa2f464d29293e7c6
frankwang <frankwang95174@gmail.com>
2023-03-04 00:22:03 +0800

    update to web hash mode

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,10 @@
-import {createRouter, createWebHistory} from 'vue-router'
+import {createRouter, createWebHashHistory, createWebHistory} from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import {getToken} from "@/common/token";
 import axios from "axios";
 
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
+    history: createWebHashHistory(),
     routes: [
         {
             path: '/login',
