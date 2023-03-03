278be992d8798610ab44d0532cced2b0f78d9253
frankwang <frankwang95174@gmail.com>
2023-03-03 13:59:14 +0800

    import bootstrap

--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ import router from './router'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
 
+import 'bootstrap'
 import './assets/all.scss'
 import VueSweetalert2 from 'vue-sweetalert2';
 import 'sweetalert2/dist/sweetalert2.min.css';
